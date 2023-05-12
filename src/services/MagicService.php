5c8aa673af71b6903ee0a88b5ce8a739481e8a26
James Edmonston <james@ethercreative.co.uk>
2023-05-12 11:49:20 +0100

    Tidy

--- src/services/MagicService.php
+++ src/services/MagicService.php
@@ -47,14 +47,16 @@ class MagicService extends Component
     {
         $settings = GraphqlAuthentication::$settings;
 
-        if ($settings->allowMagicAuthentication) {
-            $event->messages[] = [
-                'key' => 'magic_link',
-                'heading' => 'Magic Link Authentication',
-                'subject' => 'Open this link to log in to {{systemName}}',
-                'body' => "Hey {{user.friendlyName|e}},\r\n\r\nUse the following link to sign in to your account: {{siteUrl}}auth?magicCode={{code}}\r\n\r\nOr, use the following code to sign in: {{code}}.\r\n\r\nThe link and code will expire in 15 minutes.",
-            ];
+        if (!$settings->allowMagicAuthentication) {
+            return;
         }
+
+        $event->messages[] = [
+            'key' => 'magic_link',
+            'heading' => 'Magic Link Authentication',
+            'subject' => 'Open this link to log in to {{systemName}}',
+            'body' => "Hey {{user.friendlyName|e}},\r\n\r\nUse the following link to sign in to your account: {{siteUrl}}auth?magicCode={{code}}\r\n\r\nOr, use the following code to sign in: {{code}}.\r\n\r\nThe link and code will expire in 15 minutes.",
+        ];
     }
 
     /**
@@ -65,6 +67,11 @@ class MagicService extends Component
     public function registerGqlMutations(RegisterGqlMutationsEvent $event)
     {
         $settings = GraphqlAuthentication::$settings;
+
+        if (!$settings->allowMagicAuthentication) {
+            return;
+        }
+
         $userService = GraphqlAuthentication::$userService;
         $tokenService = GraphqlAuthentication::$tokenService;
         $errorService = GraphqlAuthentication::$errorService;
@@ -78,105 +85,103 @@ class MagicService extends Component
         /** @var Mailer */
         $mailerService = Craft::$app->getMailer();
 
-        if ($settings->allowMagicAuthentication) {
-            $event->mutations['sendMagicLink'] = [
-                'description' => 'Sends magic log in link. Returns string.',
-                'type' => Type::nonNull(Type::string()),
-                'args' => [
-                    'email' => Type::nonNull(Type::string()),
-                ],
-                'resolve' => function ($source, array $arguments) use ($settings, $elementsService, $usersService, $mailerService, $errorService) {
-                    $email = $arguments['email'];
-                    $user = $usersService->getUserByUsernameOrEmail($email);
-                    $message = $settings->magicLinkSent;
+        $event->mutations['sendMagicLink'] = [
+            'description' => 'Sends magic log in link. Returns string.',
+            'type' => Type::nonNull(Type::string()),
+            'args' => [
+                'email' => Type::nonNull(Type::string()),
+            ],
+            'resolve' => function ($source, array $arguments) use ($settings, $elementsService, $usersService, $mailerService, $errorService) {
+                $email = $arguments['email'];
+                $user = $usersService->getUserByUsernameOrEmail($email);
+                $message = $settings->magicLinkSent;
 
-                    if (!$user) {
-                        return $message;
-                    }
-
-                    $schemaId = GqlSchemaRecord::find()->select(['id'])->where(['name' => $settings->schemaName])->scalar();
-
-                    if ($settings->permissionType === 'multiple') {
-                        $userGroup = $user->getGroups()[0] ?? null;
-
-                        if ($userGroup) {
-                            $schemaName = $settings->granularSchemas['group-' . $userGroup->id]['schemaName'] ?? null;
-                            $schemaId = GqlSchemaRecord::find()->select(['id'])->where(['name' => $schemaName])->scalar();
-                        }
-                    }
-
-                    if (!$schemaId) {
-                        $errorService->throw($settings->invalidSchema);
-                    }
-
-                    $code = str_pad(random_int(0, 999999), 6, 0, STR_PAD_LEFT);
-
-                    $magicCodeElement = new MagicCode([
-                        'code' => $code,
-                        'userId' => $user->id,
-                        'schemaId' => $schemaId,
-                        'expiryDate' => (new DateTime())->modify('+15 mins')->format('Y-m-d H:i:s'),
-                    ]);
-
-                    if (!$elementsService->saveElement($magicCodeElement)) {
-                        $errors = $magicCodeElement->getErrors();
-                        $errorService->throw($errors[key($errors)][0]);
-                    }
-
-                    $mailerService
-                        ->composeFromKey('magic_link', [
-                            'code' => $code,
-                            'user' => $user,
-                        ])
-                        ->setTo($user)
-                        ->send();
-
+                if (!$user) {
                     return $message;
-                },
-            ];
+                }
 
-            $event->mutations['verifyMagicCode'] = [
-                'description' => 'Verifies magic log in link code. Returns user and token.',
-                'type' => Type::nonNull(Auth::getType()),
-                'args' => [
-                    'code' => Type::nonNull(Type::int()),
-                    'email' => Type::nonNull(Type::string()),
-                ],
-                'resolve' => function ($source, array $arguments) use ($settings, $tokenService, $userService, $elementsService, $usersService, $errorService) {
-                    $code = $arguments['code'];
-                    $email = $arguments['email'];
+                $schemaId = GqlSchemaRecord::find()->select(['id'])->where(['name' => $settings->schemaName])->scalar();
 
-                    $this->_clearExpiredCodes();
-                    /** @var MagicCode|null $magicCodeElement */
-                    $magicCodeElement = MagicCode::find()->where(['[[code]]' => $code])->one();
+                if ($settings->permissionType === 'multiple') {
+                    $userGroup = $user->getGroups()[0] ?? null;
 
-                    if (!$magicCodeElement) {
-                        $errorService->throw($settings->invalidMagicCode);
+                    if ($userGroup) {
+                        $schemaName = $settings->granularSchemas['group-' . $userGroup->id]['schemaName'] ?? null;
+                        $schemaId = GqlSchemaRecord::find()->select(['id'])->where(['name' => $schemaName])->scalar();
                     }
+                }
 
-                    $user = $usersService->getUserByUsernameOrEmail($email);
+                if (!$schemaId) {
+                    $errorService->throw($settings->invalidSchema);
+                }
 
-                    if (!$user) {
-                        $errorService->throw($settings->invalidMagicCode);
-                    }
+                $code = str_pad(random_int(0, 999999), 6, 0, STR_PAD_LEFT);
 
-                    if ($user->id !== $magicCodeElement->userId) {
-                        $errorService->throw($settings->invalidMagicCode);
-                    }
+                $magicCodeElement = new MagicCode([
+                    'code' => $code,
+                    'userId' => $user->id,
+                    'schemaId' => $schemaId,
+                    'expiryDate' => (new DateTime())->modify('+15 mins')->format('Y-m-d H:i:s'),
+                ]);
 
-                    $schemaId = $magicCodeElement->schemaId;
+                if (!$elementsService->saveElement($magicCodeElement)) {
+                    $errors = $magicCodeElement->getErrors();
+                    $errorService->throw($errors[key($errors)][0]);
+                }
 
-                    if (!$schemaId) {
-                        $errorService->throw($settings->invalidSchema);
-                    }
+                $mailerService
+                    ->composeFromKey('magic_link', [
+                        'code' => $code,
+                        'user' => $user,
+                    ])
+                    ->setTo($user)
+                    ->send();
 
-                    $elementsService->deleteElementById($magicCodeElement->id);
-                    $token = $tokenService->create($user, $schemaId);
+                return $message;
+            },
+        ];
 
-                    return $userService->getResponseFields($user, $schemaId, $token);
-                },
-            ];
-        }
+        $event->mutations['verifyMagicCode'] = [
+            'description' => 'Verifies magic log in link code. Returns user and token.',
+            'type' => Type::nonNull(Auth::getType()),
+            'args' => [
+                'code' => Type::nonNull(Type::int()),
+                'email' => Type::nonNull(Type::string()),
+            ],
+            'resolve' => function ($source, array $arguments) use ($settings, $tokenService, $userService, $elementsService, $usersService, $errorService) {
+                $code = $arguments['code'];
+                $email = $arguments['email'];
+
+                $this->_clearExpiredCodes();
+                /** @var MagicCode|null $magicCodeElement */
+                $magicCodeElement = MagicCode::find()->where(['[[code]]' => $code])->one();
+
+                if (!$magicCodeElement) {
+                    $errorService->throw($settings->invalidMagicCode);
+                }
+
+                $user = $usersService->getUserByUsernameOrEmail($email);
+
+                if (!$user) {
+                    $errorService->throw($settings->invalidMagicCode);
+                }
+
+                if ($user->id !== $magicCodeElement->userId) {
+                    $errorService->throw($settings->invalidMagicCode);
+                }
+
+                $schemaId = $magicCodeElement->schemaId;
+
+                if (!$schemaId) {
+                    $errorService->throw($settings->invalidSchema);
+                }
+
+                $elementsService->deleteElementById($magicCodeElement->id);
+                $token = $tokenService->create($user, $schemaId);
+
+                return $userService->getResponseFields($user, $schemaId, $token);
+            },
+        ];
     }
 
     /**
