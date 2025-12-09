b2c96bc791fc180103295d22b30ff9fb73e89198
Kyle Baker <kyle@rockitscienceagency.com>
2025-12-09 11:32:17 -0600

    removing entire check for nested Entry

--- src/services/RestrictionService.php
+++ src/services/RestrictionService.php
@@ -308,12 +308,12 @@ class RestrictionService extends Component
                     $this->restrictMutationFieldsForElement($e);
                 // } elseif ($e instanceof Entry) {
                 //     $this->_ensureValidEntry($e->id, $element->siteId);
-                } elseif ($e instanceof Entry) {
-                    if (!$e->id) {
-                        Craft::warning('Skipping Entry with null ID — likely a Matrix or Super Table block.', __METHOD__);
-                        continue;
-                    }
-                    $this->_ensureValidEntry($e->id, $element->siteId);
+                //} elseif ($e instanceof Entry) {
+                //    if (!$e->id) {
+                //        Craft::warning('Skipping Entry with null ID — likely a Matrix or Super Table block.', __METHOD__);
+                //        continue;
+                //    }
+                //    $this->_ensureValidEntry($e->id, $element->siteId);
                 } elseif ($e instanceof Asset) {
                     $this->_ensureValidAsset($e->id);
                 }
